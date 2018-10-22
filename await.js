a8b8e988e74588ebe47d3da309f107b648e97bd1
Simone Cesano <simonecesano@libero.it>
2018-10-21 22:10:50 -0700

    Still works, moving to promises

--- await.js
+++ await.js
@@ -6,21 +6,26 @@ function Step(name, requires, callback) {
     this.requiredBy = [];
     this.input  = []; 
 
-    this.callback = callback || function(){
-	var n = Math.random();
-	var t = 0;
-	me.input.forEach(function(i){ t = t + i })
-	t = t ? t : n;
-	me.result = t;
-	return t 
-    }
+    this.callback = callback ?
+	function(){
+	    me.result = callback(me.input);
+	    return me.result;
+	} : 
+	function(){
+	    var n = Math.random();
+	    var t = 0;
+	    me.input.forEach(function(i){ t = t + i })
+	    t = t ? t : n;
+	    me.result = t;
+	    return t 
+	}
     this.result = undefined;
 };
 
-function Flow(models) {
+function Flow(steps) {
     this.steps = steps || [];
     this.map = {};
-    var map;
+    var map = {};
     steps
 	.map(function(e){
 	    map[e.name] = e;
@@ -34,15 +39,19 @@ function Flow(models) {
 	    return e;
 	})
 	.map(function(e){
-	    e.promise = new Promise(function(resolve, reject){
-		var r = e.callback(e.input)
-		e.requiredBy.forEach(function(n){
-		    e.map[n].input.push(r)
+	    if (e.callback instanceof Promise) {
+		e.promise = e.callback;
+	    } else {
+		e.promise = new Promise(function(resolve, reject){
+		    var r = e.callback(e.input)
+		    e.requiredBy.forEach(function(n){
+			e.map[n].input.push(r)
+		    })
+		    resolve(r);
+		}).then(function(r){
+		    e.result = r
 		})
-		resolve(r);
-	    }).then(function(r){
-		e.result = r
-	    })
+	    }
 	    return e;
 	})
 	.map(function(e){
@@ -61,20 +70,37 @@ function Flow(models) {
 
 function test() {
     const steps = [
-	new Step('top a', []),
+	new Step('top a', [], function(){
+	    var res;
+	    var p = new Promise(function(resolve, reject) {
+		var img = new Image();
+		img.src = './air.jpg';
+		img.onload = function(){
+		    res = this;
+		    resolve(this);
+		}
+	    }).then(function(f){
+		console.log(f);
+	    })
+	    return Math.random();
+	}),
+
 	new Step('top b', []),
 	new Step('mid a', ['mid b']),
-	new Step('mid b', ['top a', 'top b']),
+	new Step('mid b', ['top a', 'top b'], function(a) {
+	    return a[0] * a[1]
+	}),
 	new Step('bottom', ['top b', 'mid b'])
     ];
 
-    var map = {};
+    var flow = new Flow(steps);
 
+    flow.steps
 	.forEach(function(e){
 	    console.log(e.name);
-	    console.log(e.result);		    
 	    console.log(e.requires);		    
 	    console.log(e.input);
+	    console.log(e.result);		    
 	})
 }
 
