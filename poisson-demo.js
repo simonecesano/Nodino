f69854c412835376395ce58be120647baeaa277d
Simone Cesano <simonecesano@libero.it>
2018-10-28 09:22:09 +0100

    Moved to SVG rendering

--- poisson-demo.js
+++ poisson-demo.js
@@ -1,19 +1,14 @@
 
 $(document).ready(function() {
 
-	// Constants
-    var	  CANVAS_WIDTH = 1200
-    , CANVAS_HEIGHT = 700;
+    // Constants
+    var	CANVAS_WIDTH = 1200,
+	CANVAS_HEIGHT = 700;
     
-    // Add canvas to page
-    var canvasElement = $("<canvas width='" + CANVAS_WIDTH + "' height='" + CANVAS_HEIGHT + "'></canvas>");
-    window.canvas = canvasElement.get(0).getContext("2d");
-    canvasElement.appendTo('body');
     
     
     
     var sampler = new PoissonDiskSampler( CANVAS_WIDTH, CANVAS_HEIGHT, 60, 10 );
-    // sampler.grid.drawGrid( canvas );
     
     var solution = sampler.sampleUntilSolution();
     console.log(solution);
@@ -36,58 +31,33 @@ $(document).ready(function() {
 	]);
     }
 
-    console.log(coordinates);
+    var svg =  document.getElementsByTagName('svg').item(0)
+    const rc = rough.svg(svg);
 
     if (0) {
-	canvas.strokeStyle = '#aaaaaa';
-	coordinates.forEach(function(e){
-	    canvas.beginPath();
-	    canvas.moveTo(e[0].x, e[0].y);
-	    canvas.lineTo(e[1].x, e[1].y);
-	    canvas.lineTo(e[2].x, e[2].y);
-	    canvas.stroke();
-	    // canvas.fill();
-	})
     } else {
-	let roughCanvas = rough.canvas(canvasElement.get(0));
-
-	
 	coordinates.forEach(function(e){
-	    var fill = ['cross-hatch', 'hachure', 'zigzag', 'dots' ][Math.floor(Math.random()*3)];
+	    // var fill = ['cross-hatch', 'hachure', 'zigzag', 'dots' ][Math.floor(Math.random()*3)];
+	    var fill = 'cross-hatch';
 	    var hachure; hachure = (fill === 'hachure') ? 4 : 8;
-	    roughCanvas.polygon([[e[0].x, e[0].y], [e[1].x, e[1].y], [e[2].x, e[2].y]],
+	    let node = rc.polygon([[e[0].x, e[0].y], [e[1].x, e[1].y], [e[2].x, e[2].y]],
 				   {
 				       stroke: '#ffffff',
 				       strokeWidth: 1,
-				       roughness: 1,
+				       roughness: 1.5,
 				       fillStyle: fill,
 				       hachureGap: hachure,
 				       hachureAngle: Math.random() * 360,
 				       fill: '#888888'
 				   });
+	    svg.appendChild(node);
 	})
 
 	solution.forEach(function(e){
-	    roughCanvas.arc (e.x, e.y, 10, 10, 0, Math.PI * 2, false, { stroke: '#444444', roughness: 1.5, strokeWidth: 1 })	    
+	    let node = rc.arc (e.x, e.y, 2, 2, 0, Math.PI * 2, false, { stroke: '#444444', roughness: 1.5, strokeWidth: 1 })	    
+	    svg.appendChild(node);
 	})
     }
 });
 
 
-
-PoissonDiskSampler.prototype.drawOutputList = function( canvas ){
-	for ( var i = 0; i < this.outputList.length; i++ ){
-		this.grid.drawPoint( this.outputList[ i ], "#444", canvas );
-	}
-}
-
-Grid.prototype.drawPoint = function( point, color, canvas ){
-	// Default color
-	color =  color || '#aaa';
-	// Draw a circle
-	canvas.beginPath();
-	// arc(x, y, radius, startAngle, endAngle, anticlockwise)
-	canvas.arc( point.x, point.y, this.pointSize, 0, 2 * Math.PI, false);
-	canvas.fillStyle = color;
-	canvas.fill();
-}
